dad77a6d14b1993a4b95d54c825c1bb7f8f346c9
kinjal.patel <kinjal.patel@solutionanalysts.com>
2016-05-12 12:03:20 +0530

    Updated android demo code to fetch dependencies from web fro SDK version 2.3.0

--- demo/src/main/java/com/tongdao/demo/MainActivity.java
+++ demo/src/main/java/com/tongdao/demo/MainActivity.java
@@ -22,6 +22,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -57,7 +58,6 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
         this.getSupportActionBar().setTitle(" 同道演示");
         this.getSupportActionBar().setBackgroundDrawable(this.getResources().getDrawable(R.drawable.bar));
         setContentView(R.layout.activity_main);
-
 
         this.inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.mainCn = (ImageView) this.findViewById(R.id.main_cn);
