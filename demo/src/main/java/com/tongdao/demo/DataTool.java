567d0ddee86bb49f7d17559704508d9df6c25a45
kinjal.patel <kinjal.patel@solutionanalysts.com>
2016-06-21 16:07:00 +0530

    updated jar to support in-app messages

--- demo/src/main/java/com/tongdao/demo/DataTool.java
+++ demo/src/main/java/com/tongdao/demo/DataTool.java
@@ -17,7 +17,8 @@ import android.net.Uri;
 
 public class DataTool {
 //    public static final String APP_KEY = "de89454e930e2257ddd96d6b4d0f48b5";
-    public static final String APP_KEY = "bc4d604fdc143433683af34b5693f444";
+public static final String APP_KEY = "2cc1c88798b3a350d16b6800dc6d3612";
+//    public static final String APP_KEY = "bc4d604fdc143433683af34b5693f444";
     public static final String BAIDU_API_KEY = "QGLHVuaavhBgygVCNVlDyRYB";
     private static ArrayList<TransferBean> transferBeanList;
     private static ArrayList<TransferRewardBean> transferRewardBeanList;
