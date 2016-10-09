f0d625f916dbeeeceb676e90a06c127ef146464b
mcs2k8 <alexeddygordo@hotmail.com>
2016-10-09 16:17:04 +0800

    Finalizing Xiaomi push demo

--- demo/src/main/java/com/tongdao/demo/xiaomi/push/MyPushMessageReceiver.java
+++ demo/src/main/java/com/tongdao/demo/xiaomi/push/MyPushMessageReceiver.java
@@ -2,6 +2,7 @@ package com.tongdao.demo.xiaomi.push;
 
 import android.content.Context;
 import android.text.TextUtils;
+import android.util.JsonWriter;
 import android.util.Log;
 
 import com.tongdao.sdk.ui.TongDaoUiCore;
@@ -10,6 +11,8 @@ import com.xiaomi.mipush.sdk.MiPushClient;
 import com.xiaomi.mipush.sdk.MiPushCommandMessage;
 import com.xiaomi.mipush.sdk.MiPushMessage;
 import com.xiaomi.mipush.sdk.PushMessageReceiver;
+
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -31,87 +34,48 @@ public class MyPushMessageReceiver extends PushMessageReceiver {
     private String mEndTime;
     @Override
     public void onReceivePassThroughMessage(Context context, MiPushMessage message) {
-        mMessage = message.getContent();
-        if(!TextUtils.isEmpty(message.getTopic())) {
-            mTopic=message.getTopic();
-        } else if(!TextUtils.isEmpty(message.getAlias())) {
-            mAlias=message.getAlias();
-        } else if(!TextUtils.isEmpty(message.getUserAccount())) {
-            mUserAccount=message.getUserAccount();
-        }
-        printDebugInfo();
+
     }
+
+    /**
+     * User clicked the notification message. Open the corresponding deeplink/link
+     * @param context
+     * @param message
+     */
     @Override
     public void onNotificationMessageClicked(Context context, MiPushMessage message) {
-        mMessage = message.getContent();
-        if(!TextUtils.isEmpty(message.getTopic())) {
-            mTopic=message.getTopic();
-        } else if(!TextUtils.isEmpty(message.getAlias())) {
-            mAlias=message.getAlias();
-        } else if(!TextUtils.isEmpty(message.getUserAccount())) {
-            mUserAccount=message.getUserAccount();
-        }
-        printDebugInfo();
+        JSONObject customContent = new JSONObject(message.getExtra());
+        String customContentString = customContent.toString();
+        TongDaoUiCore.trackOpenPushMessage(customContentString);
+        TongDaoUiCore.openPage(context, customContentString);
     }
+
     @Override
     public void onNotificationMessageArrived(Context context, MiPushMessage message) {
-        mMessage = message.getContent();
-        if(!TextUtils.isEmpty(message.getTopic())) {
-            mTopic=message.getTopic();
-        } else if(!TextUtils.isEmpty(message.getAlias())) {
-            mAlias=message.getAlias();
-        } else if(!TextUtils.isEmpty(message.getUserAccount())) {
-            mUserAccount=message.getUserAccount();
-        }
-        printDebugInfo();
+
     }
+
     @Override
     public void onCommandResult(Context context, MiPushCommandMessage message) {
-        String command = message.getCommand();
-        List<String> arguments = message.getCommandArguments();
-        String cmdArg1 = ((arguments != null && arguments.size() > 0) ? arguments.get(0) : null);
-        String cmdArg2 = ((arguments != null && arguments.size() > 1) ? arguments.get(1) : null);
-        if (MiPushClient.COMMAND_REGISTER.equals(command)) {
-            if (message.getResultCode() == ErrorCode.SUCCESS) {
-                mRegId = cmdArg1;
-            }
-        } else if (MiPushClient.COMMAND_SET_ALIAS.equals(command)) {
-            if (message.getResultCode() == ErrorCode.SUCCESS) {
-                mAlias = cmdArg1;
-            }
-        } else if (MiPushClient.COMMAND_UNSET_ALIAS.equals(command)) {
-            if (message.getResultCode() == ErrorCode.SUCCESS) {
-                mAlias = cmdArg1;
-            }
-        } else if (MiPushClient.COMMAND_SUBSCRIBE_TOPIC.equals(command)) {
-            if (message.getResultCode() == ErrorCode.SUCCESS) {
-                mTopic = cmdArg1;
-            }
-        } else if (MiPushClient.COMMAND_UNSUBSCRIBE_TOPIC.equals(command)) {
-            if (message.getResultCode() == ErrorCode.SUCCESS) {
-                mTopic = cmdArg1;
-            }
-        } else if (MiPushClient.COMMAND_SET_ACCEPT_TIME.equals(command)) {
-            if (message.getResultCode() == ErrorCode.SUCCESS) {
-                mStartTime = cmdArg1;
-                mEndTime = cmdArg2;
-            }
-        }
-        printDebugInfo();
+
     }
+
+    /**
+     * User registration result received. Forward to TongDao server
+     * @param context
+     * @param message
+     */
     @Override
     public void onReceiveRegisterResult(Context context, MiPushCommandMessage message) {
         String command = message.getCommand();
         List<String> arguments = message.getCommandArguments();
         String cmdArg1 = ((arguments != null && arguments.size() > 0) ? arguments.get(0) : null);
-        String cmdArg2 = ((arguments != null && arguments.size() > 1) ? arguments.get(1) : null);
         if (MiPushClient.COMMAND_REGISTER.equals(command)) {
             if (message.getResultCode() == ErrorCode.SUCCESS) {
                 mRegId = cmdArg1;
                 TongDaoUiCore.identifyPushToken(mRegId);
             }
         }
-        printDebugInfo();
     }
 
     /**
